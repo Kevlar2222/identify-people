f7d9160c47170c816450c34b81134a5b6137ad82
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-08-10 18:58:16 +0800

    Updates

---
+++ precache-manifest.9f2d3b45b29f8b08b5621c4a8b77cc6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b9625810ce5b9812957d83f2038a18",
+    "url": "/identify-people/index.html"
+  },
+  {
+    "revision": "2421b317cc6d1d3c803d",
+    "url": "/identify-people/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3db71d474b17a22755e0",
+    "url": "/identify-people/static/js/2.a432c983.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/identify-people/static/js/2.a432c983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2421b317cc6d1d3c803d",
+    "url": "/identify-people/static/js/main.b08c1340.chunk.js"
+  },
+  {
+    "revision": "17517f83c78f744c7bab",
+    "url": "/identify-people/static/js/runtime-main.6b4411e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/identify-people/static/media/logo.5d5d9eef.svg"
+  }
+]);
